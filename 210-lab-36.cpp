c93eb2d8892d6ff27aa94f6457639787dcbdf236
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-30 15:01:16 -0800

    Ending coding session

--- 210-lab-36.cpp
+++ 210-lab-36.cpp
@@ -5,6 +5,13 @@
 #include "IntBinaryTree.h"
 using namespace std;
 
+/* Milestones (Remove when done)
+ * 1. Project is set up and running using the provided files.
+ * 2. Code modification from int to strings is complete, and your code exercises this for testing.
+ * 3. Your code successfully creates a BST from the records and outputs it using the method of your choosing.
+ * 4. Your code's menu is functional.
+*/
+
 int main() {
     
 
