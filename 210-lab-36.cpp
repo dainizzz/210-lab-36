87c214b6bce8ddc746d5c5c6f2ee2d90393fc573
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-30 16:53:34 -0800

    10-minute update

--- 210-lab-36.cpp
+++ 210-lab-36.cpp
@@ -15,15 +15,23 @@ int main() {
     IntBinaryTree tree;
 
 	cout << "Inserting nodes into tree..." << endl;
-	tree.insertNode("apple");
-	tree.insertNode("banana");
-	tree.insertNode("cherry");
 	tree.insertNode("dragonfruit");
+	tree.insertNode("banana");
+	tree.insertNode("elderberry");
+	tree.insertNode("cherry");
+	tree.insertNode("apple");
 
 	cout << "Displaying the values in the tree..." << endl;
 	tree.displayInOrder();
-	tree.displayPostOrder();
+	cout << endl;
+
+	cout << "Deleting the value apple..." << endl;
+	tree.remove("apple");
+	cout << endl;
+
+	cout << "Displaying the updated tree..." << endl;
 	tree.displayPreOrder();
+	cout << endl;
 
     return 0;
 }
